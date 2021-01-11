cc705a79a23c6c99c818bef1e31af50073d7958f
tiroxxx <christianmarquez41@gmail.com>
2021-01-10 19:49:43 -0500

    displayed 20 pictures onto page

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const body = document.querySelector("#body");
-const image = document.createElement("img");
 const imageUrl = "https://live.staticflickr.com/"
 const queryUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ed2a2cba48899d60bb6a0f1905f5903a&safe_search=1&per_page=20&format=json&text=dog";
 
@@ -9,12 +8,14 @@ axios.get(queryUrl)
         // erasing non-json parts of the api response
         const trimmedResponse = rawResponse.split("jsonFlickrApi").pop();
         const jsonResponse = JSON.parse(trimmedResponse.substring(1, trimmedResponse.length - 1))
-        const photo = jsonResponse.photos.photo[0];
-        console.log(photo);
-        image.setAttribute("src", `${imageUrl}${photo.server}/${photo.id}_${photo.secret}.jpg`);
-        image.setAttribute("alt", "FLicker Picture");
+        const photos = jsonResponse.photos.photo;
+        console.log(photos);
 
-        body.append(image);
-
+        photos.forEach(photo => {
+            const image = document.createElement("img");
+            image.setAttribute("src", `${imageUrl}${photo.server}/${photo.id}_${photo.secret}.jpg`);
+            image.setAttribute("alt", photo.title);
+            body.append(image);
+        });
     })
 
