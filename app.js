15e8b361389420de0ce798a7c6d3326e13ae974a
tiroxxx <christianmarquez41@gmail.com>
2021-01-10 19:39:42 -0500

    adding first picture onto the page

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-let queryUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ed2a2cba48899d60bb6a0f1905f5903a&safe_search=1&per_page=20&format=json&text=dog";
+const body = document.querySelector("#body");
+const image = document.createElement("img");
+const imageUrl = "https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg"
+const queryUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ed2a2cba48899d60bb6a0f1905f5903a&safe_search=1&per_page=20&format=json&text=dog";
 
 axios.get(queryUrl)
-.then(res => {
-    const body = document.querySelector("#body");
-    let str = res.data
-    let string = str.split("jsonFlickrApi").pop();
-    let result = string.substring(1, string.length-1);
-    console.log(JSON.parse(result));
+    .then(res => {
+        const rawResponse = res.data
+        // erasing non-json parts of the api response
+        const trimmedResponse = rawResponse.split("jsonFlickrApi").pop();
+        const jsonResponse = JSON.parse(trimmedResponse.substring(1, trimmedResponse.length - 1))
+        const photo = jsonResponse.photos.photo[0];
+        console.log(photo);
+        image.setAttribute("src", `${imageUrl}${photo}`);
 
 
-})
+
+    })
 
