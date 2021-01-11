c9c1a73cfc30e4a11ae046e5e5d9df2da1b4c6a9
tiroxxx <christianmarquez41@gmail.com>
2021-01-10 22:32:07 -0500

    search function called when form is submitted on when button is pressed

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const body = document.querySelector("#body");
 const button = document.querySelector("#button");
 const form = document.querySelector("#form");
 const formInput = document.querySelector("#input");
-
-const imageUrl = "https://live.staticflickr.com/"
-const queryUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ed2a2cba48899d60bb6a0f1905f5903a&safe_search=1&per_page=20&format=json&text=dog";
+const imageUrl = "https://live.staticflickr.com/";
 
 button.addEventListener("click", e => {
     e.preventDefault();
     console.log(formInput.value);
+    search(formInput.value);
+    formInput.value = "";
+    
 });
 
-button.addEventListener("submit", e => {
+form.addEventListener("submit", e => {
     e.preventDefault();
     console.log(formInput.value);
+    search(formInput.value);
+    formInput.value = "";
 });
 
-function apiCall() {
+function search(keyword) {
+    const queryUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&
+    api_key=ed2a2cba48899d60bb6a0f1905f5903a&safe_search=3&per_page=20&format=json&text=${keyword}`;
+
     axios.get(queryUrl)
         .then(res => {
             const rawResponse = res.data
@@ -25,7 +31,7 @@ function apiCall() {
             const jsonResponse = JSON.parse(trimmedResponse.substring(1, trimmedResponse.length - 1))
             const photos = jsonResponse.photos.photo;
             console.log(photos);
-
+            // append all pictures to the page
             photos.forEach(photo => {
                 const image = document.createElement("img");
                 image.setAttribute("src", `${imageUrl}${photo.server}/${photo.id}_${photo.secret}.jpg`);
